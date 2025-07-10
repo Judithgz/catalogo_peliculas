1a0dd2697fce5ce5029db1956a5735574e5e194e
Judithgz <judithnarvaez27@gmail.com>
2025-07-09 23:53:40 -0500

    Initial commit

---
+++ test_catalogo.py
@@ -0,0 +1,26 @@
+from dominio.Pelicula import Pelicula
+from servicio.catalogo_peliculas import CatalogoPeliculas as cp
+opcion = None
+while opcion != 4:
+    try:
+        print('Opciones: ')
+        print('1. Agregar Pelicula')
+        print('2. Mostrar Peliculas')
+        print('3. Eliminar Catalogo de Peliculas')
+        print('4. Salir')
+        opcion = int(input('Escribe tu opcion (1-4): '))
+
+        if opcion == 1:
+            nombre_pelicula = input('Escribe el nombre de la pelicula: ')
+            pelicula = Pelicula(nombre_pelicula)
+            cp.agregar_pelicula(pelicula)  #cp -> catalogo_peliculas
+        elif opcion == 2:
+            cp.mostrar_peliculas()
+        elif opcion == 3:
+            cp.eliminar_peliculas()
+
+    except Exception as e:
+        print(f'Ocurrio un error {e}')
+        opcion = None
+else:
+    print('Salimos del programa...')
